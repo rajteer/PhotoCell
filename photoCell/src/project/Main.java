c75b188762ce61179d00ebc6d3f524340cea591f
Paweł Rajter <prajter00@gmail.com>
2021-04-03 17:10:11 +0200

    Dodanie funkcjonalnosci w klasie Main

--- photoCell/src/project/Main.java
+++ photoCell/src/project/Main.java
@@ -1,8 +1,13 @@
 package project;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main
+{	
 	public static void main(String[] args)
-	{
+	{	
+		ElectroMagneticWave wave = new ElectroMagneticWave(380);
+		
 		PhotoEffect window = new PhotoEffect();
 		window.setVisible(true);
 	}
